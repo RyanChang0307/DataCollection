391cf017a4454a844813fa23d70e1ee24560a43b
ryant.chang <stannis0307@hotmail.com>
2016-09-06 08:31:23 +0800

    Add new project

---
+++ Enmos_BatchIDMapping_DataCollection/BatchIDMapping.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LINQtoCSV;
+
+namespace Enmos_BatchIDMapping_DataCollection
+{
+    public class BatchIDMapping
+    {
+        private string _Site;
+        private string _BatchId;
+        private string _BatchNo;
+        private DateTime _Time;
+
+        [CsvColumn(FieldIndex = 1, CanBeNull = false)]
+        public string Site
+        {
+            get
+            {
+                return _Site;
+            }
+
+            set
+            {
+                _Site = value;
+            }
+        }
+
+        [CsvColumn(FieldIndex = 2, CanBeNull = false)]
+        public string BatchId
+        {
+            get
+            {
+                return _BatchId;
+            }
+
+            set
+            {
+                _BatchId = value;
+            }
+        }
+
+        [CsvColumn(FieldIndex = 3, CanBeNull = false)]
+        public string BatchNo
+        {
+            get
+            {
+                return _BatchNo;
+            }
+
+            set
+            {
+                _BatchNo = value;
+            }
+        }
+
+        [CsvColumn(FieldIndex = 4, OutputFormat = "yyyy-MM-dd HH:mm:ss ", CanBeNull = true)]
+        public DateTime Time
+        {
+            get
+            {
+                return _Time;
+            }
+
+            set
+            {
+                _Time = value;
+            }
+        }
+
+
+    }
+}
---
+++ Enmos_BatchIDMapping_DataCollection/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using LINQtoCSV;
+using HadoopUtil;
+using Dapper;
+using System.Data.Odbc;
+
+namespace Enmos_BatchIDMapping_DataCollection
+{
+    class Program
+    {
+        static string site01ConStr = ConfigurationManager.ConnectionStrings["ENMOS01"].ConnectionString;
+        static string site02ConStr = ConfigurationManager.ConnectionStrings["ENMOS02"].ConnectionString;
+        static void Main(string[] args)
+        {
+            List<BatchIDMapping> data = getDataFromDB("001");
+            var list = data.Union(getDataFromDB("002"));
+            ConvertDataToCsv(list.ToList());
+        }
+
+        #region Sql Command
+
+        static string getSqlCmd(string site)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(string.Format(" SELECT '{0}' Site,BatchId, [Value] AS BatchNo, [Time] FROM Monitoring.BatchParams AS A ", site));
+            sb.Append(" WHERE ([Index] = 0) AND ([Value] LIKE 'MD%') AND ([Time] BETWEEN '2016-07-14' AND GETDATE() - 1)");
+            return sb.ToString();
+
+        }
+
+        #endregion
+
+        #region Retrieve data from database
+
+        static List<BatchIDMapping> getDataFromDB(string site)
+        {
+            string connStr = site == "001" ? site01ConStr : site02ConStr;
+            var cn = new SqlConnection(connStr);
+            cn.Open();
+            string sqlcmd = getSqlCmd(site);
+            return cn.Query<BatchIDMapping>(sqlcmd).ToList();
+        }
+
+
+        #endregion
+
+        #region PutDataToHDFS
+
+        static void PutDataToHDFS(string srcPath, string srcFileName, string trgtFilePath)
+        {
+            HdfsUtil data = new HdfsUtil(new Uri("http://master02:50070"), "root");
+            if (data.IsFileExist(trgtFilePath, srcFileName))
+                data.DeleteFile(string.Format("{0}/{1}", trgtFilePath, srcFileName));
+            data.PutDataToHdfs(string.Format("{0}\\{1}", srcPath, srcFileName), string.Format("{0}/{1}", trgtFilePath, srcFileName));
+            RefreshTblSchema();
+        }
+
+
+        #endregion
+
+        #region Refresh Table Schema
+
+        static void RefreshTblSchema()
+        {
+            OdbcConnection con = new OdbcConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["Impala_ODBC"].ConnectionString;
+            con.Open();
+            string sqlcmd = "REFRESH enmos_batchid_mapping";
+            OdbcCommand cmd = new OdbcCommand(sqlcmd, con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+
+        #endregion
+
+        #region Convert Data To CSV and Put data to hdfs
+
+        /// <summary>
+        /// Convert Data To CSV
+        /// </summary>
+        /// <param name="data"></param>
+        static void ConvertDataToCsv(List<BatchIDMapping> data)
+        {
+            CsvFileDescription outputFileDescription = new CsvFileDescription
+            {
+                SeparatorChar = ','
+                ,FirstLineHasColumnNames = false
+                ,EnforceCsvColumnAttribute = true
+            };
+            CsvContext cc = new CsvContext();
+            string filename = "Enmos_BatchId_Mapping.csv";
+            cc.Write(data, string.Format("C:\\DataCollection\\{0}", filename), outputFileDescription);
+            PutDataToHDFS("C:\\DataCollection", filename, "/DataWarehouse/ENMOS/Enmos_BatchId_Mapping");
+        }
+
+        #endregion
+
+
+
+
+
+    }
+}
