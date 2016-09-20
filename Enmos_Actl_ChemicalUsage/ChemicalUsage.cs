dd872cef5c835afd8acba3bdd831f82ebcd46cb9
ryant.chang <stannis0307@hotmail.com>
2016-09-20 17:32:01 +0800

    new job

---
+++ Enmos_Actl_ChemicalUsage/ChemicalUsage.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LINQtoCSV;
+
+namespace Enmos_Actl_ChemicalUsage
+{
+  public  class ChemicalUsage
+    {
+        private string _SiteID;
+        private int _BatchOrderID;
+        private string _BatchOrderNo;
+        private int _Machine;
+        private string _ProductCode;
+        private string _ProductName;
+        private float _ProductWeight;
+        private float _FabricWeight;
+        private int _TypeID;
+        private string _ProductType;
+
+        [CsvColumn(FieldIndex = 1, CanBeNull = false)]
+        public int BatchOrderID
+        {
+            get
+            {
+                return _BatchOrderID;
+            }
+
+            set
+            {
+                _BatchOrderID = value;
+            }
+        }
+
+        [CsvColumn(FieldIndex = 2, CanBeNull = false)]
+        public string BatchOrderNo
+        {
+            get
+            {
+                return _BatchOrderNo;
+            }
+
+            set
+            {
+                _BatchOrderNo = value;
+            }
+        }
+        [CsvColumn(FieldIndex = 3, CanBeNull = true)]
+        public int Machine
+        {
+            get
+            {
+                return _Machine;
+            }
+
+            set
+            {
+                _Machine = value;
+            }
+        }
+        [CsvColumn(FieldIndex = 4, CanBeNull = true)]
+        public string ProductCode
+        {
+            get
+            {
+                return _ProductCode;
+            }
+
+            set
+            {
+                _ProductCode = value;
+            }
+        }
+        [CsvColumn(FieldIndex = 5, CanBeNull = true)]
+        public string ProductName
+        {
+            get
+            {
+                return _ProductName;
+            }
+
+            set
+            {
+                _ProductName = value;
+            }
+        }
+        [CsvColumn(FieldIndex = 6, CanBeNull = true)]
+        public float ProductWeight
+        {
+            get
+            {
+                return _ProductWeight;
+            }
+
+            set
+            {
+                _ProductWeight = value;
+            }
+        }
+        [CsvColumn(FieldIndex = 7, CanBeNull = true)]
+        public float FabricWeight
+        {
+            get
+            {
+                return _FabricWeight;
+            }
+
+            set
+            {
+                _FabricWeight = value;
+            }
+        }
+        [CsvColumn(FieldIndex = 8, CanBeNull = true)]
+        public int TypeID
+        {
+            get
+            {
+                return _TypeID;
+            }
+
+            set
+            {
+                _TypeID = value;
+            }
+        }
+        [CsvColumn(FieldIndex = 9, CanBeNull = true)]
+        public string ProductType
+        {
+            get
+            {
+                return _ProductType;
+            }
+
+            set
+            {
+                _ProductType = value;
+            }
+        }
+        [CsvColumn(FieldIndex = 10, CanBeNull = false)]
+        public string SiteID
+        {
+            get
+            {
+                return _SiteID;
+            }
+
+            set
+            {
+                _SiteID = value;
+            }
+        }
+    }
+}
--- Enmos_Actl_ChemicalUsage/Program.cs
+++ Enmos_Actl_ChemicalUsage/Program.cs
@@ -1,5 +1,11 @@
-﻿using System;
+﻿using Dapper;
+using HadoopUtil;
+using LINQtoCSV;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Odbc;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +14,101 @@ namespace Enmos_Actl_ChemicalUsage
 {
     class Program
     {
+        static string site01ConStr = ConfigurationManager.ConnectionStrings["ENMOS01"].ConnectionString;
+        static string site02ConStr = ConfigurationManager.ConnectionStrings["ENMOS02"].ConnectionString;
         static void Main(string[] args)
         {
+            List<ChemicalUsage> list = getDataFromDB("001");
+            ConvertDataToCsv(list, "001");
+            List<ChemicalUsage> list2 = getDataFromDB("002");
+            ConvertDataToCsv(list2, "002");
+            RefreshTblSchema();
+
         }
+
+        #region Sql Command
+
+        static string getSqlCmd(string site)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(" SELECT   boh.Id AS BatchOrderId,boh.BatchOrderNo,Machine.GetMachineNameByMachineNo(boh.MachineNo) AS Machine,bod.ProductCode,p.Desc1 AS ProductName, SUM(bod.ProductWeight) AS ProductWeight");
+            sb.Append(string.Format(" , boh.FabricWeight, p.TypeID, (Select Code From General.luProductType Where LanguageId = @@langId And [Index] = p.TypeID) AS ProductType,'{0}' SiteID", site));
+            sb.Append(" FROM Recipe.BatchOrderHeader boh INNER JOIN Recipe.BatchOrderDetails bod ON bod.BatchOrderHeaderId = boh.Id");
+            sb.Append(" INNER JOIN Recipe.Products p ON p.Code = bod.ProductCode INNER JOIN General.luProductType lpt ON lpt.[Index] = p.TypeID  AND LanguageId = @@langId");
+            sb.Append(" GROUP BY boh.Id, boh.BatchOrderNo, boh.MachineNo, bod.ProductCode, p.Desc1, boh.FabricWeight, p.TypeID");
+            return sb.ToString();
+
+        }
+
+        #endregion
+
+        #region Retrieve data from database
+
+        static List<ChemicalUsage> getDataFromDB(string site)
+        {
+            string connStr = site == "001" ? site01ConStr : site02ConStr;
+            var cn = new SqlConnection(connStr);
+            cn.Open();
+            string sqlcmd = getSqlCmd(site);
+            return cn.Query<ChemicalUsage>(sqlcmd).ToList();
+        }
+
+
+        #endregion
+
+        #region PutDataToHDFS
+
+        static void PutDataToHDFS(string srcPath, string srcFileName, string trgtFilePath)
+        {
+            HdfsUtil data = new HdfsUtil(new Uri("http://master01:50070"), "root");
+            if (data.IsFileExist(trgtFilePath, srcFileName))
+                data.DeleteFile(string.Format("{0}/{1}", trgtFilePath, srcFileName));
+            data.PutDataToHdfs(string.Format("{0}\\{1}", srcPath, srcFileName), string.Format("{0}/{1}", trgtFilePath, srcFileName));
+            //RefreshTblSchema();
+        }
+
+
+        #endregion
+
+        #region Convert Data To CSV and Put data to hdfs
+
+        /// <summary>
+        /// Convert Data To CSV
+        /// </summary>
+        /// <param name="data"></param>
+        static void ConvertDataToCsv(List<ChemicalUsage> data, string site)
+        {
+            CsvFileDescription outputFileDescription = new CsvFileDescription
+            {
+                SeparatorChar = ','
+                ,
+                FirstLineHasColumnNames = false
+                ,
+                EnforceCsvColumnAttribute = true
+            };
+            CsvContext cc = new CsvContext();
+            string filename = string.Format("Enmos_Actl_ChemicalUsage_{0}.csv", site);
+            cc.Write(data, string.Format("C:\\DataCollection\\{0}", filename), outputFileDescription);
+            PutDataToHDFS("C:\\DataCollection", filename, "/DataWarehouse/ENMOS/Enmos_Actl_ChemicalUsage");
+        }
+
+        #endregion
+
+        #region Refresh Table Schema
+
+        static void RefreshTblSchema()
+        {
+            OdbcConnection con = new OdbcConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["Impala_ODBC"].ConnectionString;
+            con.Open();
+            string sqlcmd = "REFRESH Enmos_Actl_ChemicalUsage";
+            OdbcCommand cmd = new OdbcCommand(sqlcmd, con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+
+        #endregion
+
     }
 }
